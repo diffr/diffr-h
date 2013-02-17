6fb08fd297dee37f4b014b9e1d9dcb373b846558
William Martin <will.st4@gmail.com>
2013-02-17 18:54:15 +0000

    Added install guide.
    Ignored idea projects.
    Ignored dist directory.
    Added documentation headers.

--- diff/Main.hs
+++ diff/Main.hs
@@ -1,3 +1,25 @@
+{- |
+ Module      :  Diffr.Main
+ Description :  Main entry point for diffr.
+ Since       :  0.0
+ Authors     :  William Martin
+ License     :  This file is part of diffr-h.
+
+ diffr-h is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ diffr-h is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ You should have received a copy of the GNU General Public License
+ along with diffr-h.  If not, see <http://www.gnu.org/licenses/>.
+-}
+
+module Diffr.Main where
+
 import System.Environment
 
 -- | 'main' runs the main program
